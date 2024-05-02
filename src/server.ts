4d2892dcebf15fe0d039fc5fe0e4cfb0d20537f0
orhan.calik <orhan.calik@student.ap.be>
2024-05-02 20:03:40 +0000

    css gefixt

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const PORT = 3000;
 
 
 // Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
 
