840ab1cc9f136805d7618c2bc7e0199ff083498b
orhan.calik <orhan.calik@student.ap.be>
2024-05-05 19:18:56 +0000

    MONGODB implentatie succesvol

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,77 @@
 // Importeer de benodigde modules
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import footballersData from '../data/footballers.json';
 import clubsData from '../data/clubs.json';
+import { MongoClient, Db } from 'mongodb';
+import dotenv from 'dotenv';
 // Maak een Express-applicatie
 const app = express();
 const PORT = 3000;
 
-
+dotenv.config();
 
 // Middleware
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '..', 'views'));
 
+const mongoURI: string | undefined = process.env.MONGO_URI ?? '';
+const dbName = process.env.DB_NAME;
+const port = process.env.PORT;
+
 // Welkomstpagina route
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+const client = new MongoClient(mongoURI);
+let db: Db;
+
+async function connectToMongoDB() {
+    try {
+        await client.connect();
+        console.log('Verbonden met MongoDB');
+        db = client.db(dbName);
+    } catch (error) {
+        console.error('Fout bij het verbinden met MongoDB:', error);
+    }
+}
+
+async function importFootballersDataToMongoDB() {
+    try {
+        // Controleer of de data al is geïmporteerd door te zoeken naar een document met 'imported: true'
+        const collection = db.collection('footballers');
+
+        // Logging toevoegen om te controleren of de MongoDB-collectie correct wordt geopend
+        console.log('Collection:', collection);
+
+        const isImported = await collection.findOne({ imported: true });
+
+        // Logging toevoegen om te controleren of 'isImported' correct wordt ingesteld
+        console.log('Is imported:', isImported);
+
+        if (!isImported) {
+            // Als de data nog niet is geïmporteerd, importeer deze dan
+            const footballersDataPath = path.join(__dirname, '../data/footballers.json');
+            const footballersData = await fs.promises.readFile(footballersDataPath, 'utf-8');
+            const footballers = JSON.parse(footballersData);
+
+            // Voeg de geïmporteerde veld 'imported: true' toe aan elk document
+            footballers.forEach((footballer: any) => footballer.imported = true);
+
+            // Voeg de gegevens toe aan de MongoDB-collectie
+            const result = await collection.insertMany(footballers);
+            console.log(`${result.insertedCount} documents were inserted into the footballers collection.`);
+        } else {
+            console.log('Data is al geïmporteerd. Overslaan...');
+        }
+    } catch (error) {
+        console.error('Error importing footballers data to MongoDB:', error);
+    }
+}
+connectToMongoDB().then(() => importFootballersDataToMongoDB());
 // Dummy data
 
 
